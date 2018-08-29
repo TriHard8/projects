7d8152be9bcc886b5f88e199eb81e48250aa1712
Trihard8 <hall@Matts-MacBook-Air.local>
2018-08-29 15:37:03 -0400

    cron upload

---
+++ utils/my_utils.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+import bs4 as bs
+import requests
+from datetime import date, timedelta
+import urllib.request
+
+def get_script_directory():
+    directory = os.path.abspath(os.path.dirname(sys.argv[0]))
+    return directory
+
+def up_x_dir(base_directory, num):
+    #Assumes / is the base directory in all cases
+    slashes = base_directory.count('/')
+    if num > slashes:
+        raise RuntimeError("Attempting to go up more levels than available")
+    elif num == slashes:
+        return '/'
+    else:
+        regex = "(.+)"
+        for i in range (0,num):
+            regex += "/.+"
+        regex += "$"
+        m = re.match(regex, base_directory)
+        if m:
+            return (m.group(1))
+
+def download_file(link, save_location):
+    urllib.request.urlretrieve(link, save_location)
+    return
+ 
+def date_for_files():
+    return date.today().strftime("%Y%m%d")
+
+def get_soup(site):
+    r = requests.get(site)
+    soup = bs.BeautifulSoup(r.content, 'html.parser')
+    return soup
+ 
+def get_soup_str(html):
+    return bs.BeautifulSoup(html, 'html.parser')
+
+def list_2_csv(my_list):
+    csv = ""
+    for item in my_list:
+        for i in range(0,len(item)):
+            if i < (len(item)-1):
+                csv = csv + "{0},".format(item[i])
+            else:
+                csv = csv + "{0}\n".format(item[i])
+
+    return csv
+
+if __name__ == "__main__":
+    this = get_script_directory()
+    up_x_dir(this, 1)
