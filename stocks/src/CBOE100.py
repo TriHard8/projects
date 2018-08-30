986adb7a528dd6de3f903de6d46999c3e23c64b2
Trihard8 <matthew.hall8@gmail.com>
2018-08-30 01:32:56 -0400

    using a full list of s&p 100

---
+++ stocks/src/CBOE100.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import sys
+import re
+import requests
+from bs4 import BeautifulSoup as bsoup
+
+output_file = "{0}/repo/projects/stocks/data/sp100_stocks.txt".format(os.path.expanduser('~'))
+sp_100 = open(output_file, 'w+')
+    
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36'}
+url = "https://www.marketvolume.com/quotes/index_components.asp?s=SP100"
+r = requests.get(url, headers=headers)
+soup = bsoup(r.content, 'html.parser')
+
+company=""
+for record in soup.findAll('tr'):
+    company = ""
+    for data in record.findAll('td'):
+        company = company + "," + data.text
+    m = re.match(r'^,([a-zA-Z\.]+,.+),.+,.+$', company)
+    if m: company = m.groups()[0]
+    sp_100.write("{}\n".format(company))
+
--- stocks/src/fidelity_login.py
+++ stocks/src/fidelity_login.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-configfile = "/Users/hall/repo/projects/utils/"
 import os
 import sys
+configfile = "{0}/repo/projects/utils/".format(os.path.expanduser('~'))
 sys.path.append(os.path.dirname(os.path.expanduser(configfile)))
 import my_utils as my
 import time
@@ -15,7 +15,7 @@ with open("{0}/password".format(os.path.expanduser('~')), 'r') as f:
     user = f.readline().rstrip('\n')
     pwd = f.readline().rstrip('\n')
 
-stock_list = "/Users/hall/repo/projects/stocks/data/sp100_stocks.txt"
+stock_list = "{0}/repo/projects/stocks/data/sp100_stocks.txt".format(os.path.expanduser('~')) 
 
 driver = webdriver.Chrome()
 driver.get("https://www.fidelity.com/login/accountposition?AuthRedUrl=https://oltx.fidelity.com/ftgw/fbc/ofsummary/defaultPage&AuthOrigUrl=https://scs.fidelity.com/customeronly/accountposition.shtml")
@@ -31,12 +31,14 @@ time.sleep(5)
 option_page = "https://researchtools.fidelity.com/ftgw/mloptions/goto/optionChain?symbols="
 
 with open(stock_list, 'r') as f:
+    next(f)
     for line in f:
-        symbol = line.rstrip('\n')
+        symbol = line.strip().split(',')[0]
+        
         driver.get("{0}{1}".format(option_page, symbol))
         innerHTML = driver.execute_script("return document.body.innerHTML")
         soup = my.get_soup_str(innerHTML)
-        #print(soup)
         for price in soup.find_all("span", {"class" : "main-number"}):
             print("{0} : {1}".format(symbol, price.string[1:]))
+
 #driver.close()
