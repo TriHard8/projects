4634f4ea5d0b6f60f9dc24a45a9c735d923c31c1
Matt <trihard8@localhost.localdomain>
2019-06-25 10:04:49 -0400

    reversed the bracket, but need to reverse at each level

--- tennis/src/tournament.cpp
+++ tennis/src/tournament.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::cout;
@@ -16,6 +17,8 @@ class TennisPlayer{
     public:
         TennisPlayer(string player, int chances): name(player),odds(chances) {};
         TennisPlayer(string player): name(player) {};
+        TennisPlayer(const TennisPlayer &rhs): name(rhs.name),odds(rhs.odds) {};
+        TennisPlayer(): name(""),odds(0) {};
         ~TennisPlayer() {}
 
         friend std::ostream& operator<<(std::ostream &, const TennisPlayer &);
@@ -35,68 +38,49 @@ class TennisPlayer{
         bool operator==(const TennisPlayer &rhs) const{ return this->getOdds() == rhs.getOdds(); }
         bool operator!=(const TennisPlayer &rhs) const{ return !(*this == rhs); }
 };
-class TournamentNode{
-    private:
-        TennisPlayer player;
-        uint position;
-    
-    public:
-        TournamentNode(string name, int odds): player(TennisPlayer(name, odds)) {};
-        TournamentNode(string name): player(TennisPlayer(name)) {}; 
-        TournamentNode(): player(TennisPlayer("")) {};
-        ~TournamentNode() {};
-
-        //gettors
-        TennisPlayer getPlayer() const { return player; }
-
+struct Node{
+    TennisPlayer data;
+    Node *left, *right;
+};
+Node* newNode(TennisPlayer data){
+    Node* node = new Node();
+    node->left = node->right = NULL;
+    return node;
 };
 class Tournament{
     private:
         uint size;
-        std::vector<TournamentNode> bracket;
+        std::vector<TennisPlayer> bracket;
         
     public:
         Tournament(int);
         ~Tournament() {};
 
         void Insert(const string &);
-        friend std::ostream& operator<<(std::ostream &, const Tournament &);
-        
+        friend std::ostream& operator<<(std::ostream &, Tournament &);
+        Node* insertLevelOrder(Node*, int);     
 };
 int main(){
 
     Tournament tourney(32);
     cout << tourney;
     TennisPlayer first("Matt", 1600), second("Hall", 300);
-    if(first > second) cout << first.getName() << endl;
+    if(first < second) cout << first.getName() << endl;
     else cout << second.getName() << endl;
 
     return 0;
 }
-std::ostream& operator<<(std::ostream &os, const Tournament &rhs){
-    int i(1), j(1), add(1);
-    int levels(0), level(0);
-    levels = log(float(brackets.size()))/log(float(2));
-    bool up(true);
-    for(; i < rhs.bracket.size(); ++i){
-        if( i%2 == 1){
-            os << rhs.bracket[j++].getPlayer() << "\n";
-        }
-        else{
-            for(int k(0); k < add; ++k) cout << "\t\t";
-            if(up){
-                if (add = bracket.size()-1){
-                    up = false;
-                    add--;
-                }
-                else{
-                    add++
-                }
-            }
-            os << rhs.bracket
-        }
-    }
+std::ostream& operator<<(std::ostream &os, Tournament &rhs){
+//    Node* champion = rhs.insertLevelOrder(champion, 0); 
+    for(int i(1); i < rhs.bracket.size(); ++i) os << rhs.bracket[i] << endl;
     return os;
+}
+Node* Tournament::insertLevelOrder(Node* root, int i){
+    return NULL;
+//    if(i < bracket.size()){
+//        Node* temp = 
+//    }
+
 }
 std::ostream& operator<<(std::ostream &os, const TennisPlayer &rhs){
     os << rhs.getName() << " (";
@@ -109,12 +93,13 @@ Tournament::Tournament(int a){
     size = a*2 - 1;
     bracket.resize(a*2);    
 
-    for(int i(1); i < a+1; ++i) bracket[i] = TournamentNode(std::to_string(i), i*100);
+    for(int i(1); i < a+1; ++i) bracket[i] = TennisPlayer(std::to_string(i), i*100);
     
     int j(1);
     for(int i(a+1); i < bracket.size(); ++i){
-        if(bracket[j].getPlayer() < bracket[j+1].getPlayer()) bracket[i] = bracket[j];
+        if(bracket[j] < bracket[j+1]) bracket[i] = bracket[j];
         else bracket[i] = bracket[j+1];
         j += 2; 
     }
+    std::reverse(bracket.begin(), bracket.end());
 }
