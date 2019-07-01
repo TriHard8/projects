0cbfa1c466e67fd00310167befb219b5c2f34cbb
Trihard8 <matthew.hall8@gmail.com>
2019-06-30 21:04:56 -0400

    Added file reading to the test file. Need to implement into Tournament

--- tennis/src/test.cpp
+++ tennis/src/test.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <stdlib.h>
+#include <limits.h>
+#include <sstream>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    int size = 16;
-    vector<int> fill;
-    vector<int>::iterator it1, it2;
-    for(int i(0); i < (size * 2); ++i)
-        fill.push_back(i); 
-
-    it1 = fill.begin() + 1;
-    it2 = fill.begin() + size + 1;
-    while(size > 1){
-        reverse(it1, it2);
-        it1 += size;
-        it2 += (size / 2);
-        size /= 2;
+    vector<string> row;
+    fstream ifile;
+    string temp, line, word;
+    char actualpath [PATH_MAX];
+    char *filepath;
+    filepath = realpath(argv[1], actualpath);
+    ifile.open(filepath, ios::in);
+    
+    while(getline(ifile, line)){
+        row.clear();
+        stringstream s(line);
+        while(getline(s, word, ',')){
+            row.push_back(word);
+        } 
     }
-    for(int i(0); i < fill.size(); ++i)
-        cout << fill[i] << " ";
-    cout << endl;
-
+    
     return 0;
 }
