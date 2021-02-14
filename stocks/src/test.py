79f53f3d98ea91d3dbf6563e8c5aae7e547a0112
Matt <trihard8@gamingPC.myfiosgateway.com>
2021-02-13 19:39:40 -0500

    Starting to multithread

--- stocks/src/test.py
+++ stocks/src/test.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
+from multiprocessing import Pool
+from multiprocessing import Process
+import os, psutil
 
-symbols = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-numfiles = 8
-high = int( len(symbols) / numfiles )
-high
+def f(x):
+    #print(x)
+    #print('module name:', __name__)
+    #print('parent process: ', os.getppid())
+    pid = os.getpid()
+    print('process id', pid)
+    py = psutil.Process(pid)
+    print(py.memory_info())
+    return x*x
 
-for num in range(numfiles):
-    if num == numfiles-1:
-            for symbol in symbols[high*num:]:
-                    print(symbol, end=" : ")
-            print()
-    else:
-            for symbol in symbols[high*num:high*(num+1)]:
-                    print(symbol, end=" : ")
-            print()
+
+if __name__ == '__main__':
+    with Pool(6) as p:
+        p.map(f, [1, 2, 3, 4, 5, 6])
+
+#    p = Process(target = f, args = [5])
+#    p.start()
+#    p.join()
