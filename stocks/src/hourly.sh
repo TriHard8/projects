85bf8f3272a1353146716eae4a24747eff5bdc89
Matt <trihard8@gamingPC.myfiosgateway.com>
2021-03-19 19:20:26 -0400

    automate running daily fidelity

--- stocks/src/hourly.sh
+++ stocks/src/hourly.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 hour=`date +%Y%m%d%H`
-mkdir "/home/trihard8/repo/projects/stocks/data/${hour:0:8}"
+DIR="/home/trihard8/repo/projects/stocks/data/${hour:0:8}"
+if [ ! -d $DIR ]
+then
+    mkdir $DIR
+fi
+if [ "${hour:8:2}" -ge "16" ]
+then
+    hour="${hour:0:8}16" 
+    echo $hour 
+fi
 file="/home/trihard8/repo/projects/stocks/data/${hour:0:8}/${hour}_all${1}_fidelity_option_output.csv"
 
 /home/trihard8/repo/projects/stocks/src/fidelity_login.py "${1}" >> $file
+
+/home/trihard8/repo/projects/stocks/src/missingSymbols.py "/home/trihard8/repo/projects/stocks/data/options_stocks${1}.csv" $file
--- stocks/src/multirun.sh
+++ stocks/src/multirun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..9}
+for i in {1..8}
 do
     time /home/trihard8/repo/projects/stocks/src/hourly.sh ${i} &
     sleep 30
