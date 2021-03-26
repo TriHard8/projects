2bcc7a06c95512556988510fe475bcdd0e4db1a5
Matt <trihard8@gamingPC.myfiosgateway.com>
2021-03-26 10:06:40 -0400

    headless testing accompanying test scripts

--- stocks/src/hourly.sh
+++ stocks/src/hourly.sh
@@ -13,6 +13,6 @@ then
 fi
 file="/home/trihard8/repo/projects/stocks/data/${hour:0:8}/${hour}_all${1}_fidelity_option_output.csv"
 
-/home/trihard8/repo/projects/stocks/src/fidelity_login.py "${1}" >> $file
+/home/trihard8/repo/projects/stocks/src/fidelity_login.py.working "${1}" >> $file
 
 /home/trihard8/repo/projects/stocks/src/missingSymbols.py "/home/trihard8/repo/projects/stocks/data/options_stocks${1}.csv" $file
